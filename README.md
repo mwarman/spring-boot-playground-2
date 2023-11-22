9fdddcd8791af854259ab6bc900ac9ee6f55a20b
Matthew Warman <warmanmr@gmail.com>
2023-11-22 06:40:33 -0500

    springdoc openapi documentation

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ For further reference, please consider the following sections:
 * [Official Gradle documentation](https://docs.gradle.org)
 * [Spring Boot Gradle Plugin Reference Guide](https://docs.spring.io/spring-boot/docs/2.7.17/gradle-plugin/reference/html/)
 * [Create an OCI image](https://docs.spring.io/spring-boot/docs/2.7.17/gradle-plugin/reference/html/#build-image)
+* [SpringDoc OpenAPI v1](https://springdoc.org/v1/)
 
 ### Additional Links
 These additional references should also help you:
--- src/main/java/net/leanstacks/demo/todo/CreateTodoDto.java
+++ src/main/java/net/leanstacks/demo/todo/CreateTodoDto.java
@@ -1,18 +1,26 @@
 package net.leanstacks.demo.todo;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public class CreateTodoDto {
-	
+
+	@NotBlank(message = "title is required")
+	@Size(min = 1, max = 100, message = "title must contain 1 to 100 characters")
 	private String title;
-	
+
 	public CreateTodoDto() {
-		
+
 	}
-	
+
 	public CreateTodoDto(String title) {
 		this();
 		this.title = title;
 	}
 
+	@Schema(example = "Document the API")
 	public String getTitle() {
 		return title;
 	}
